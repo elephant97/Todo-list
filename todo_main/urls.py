dbee69a71d5a0c5d7078524fab4f458c8eba1e69
elephant97 <elephant0908@gmail.com>
2022-01-09 20:12:43 +0900

    first commit

---
+++ todo_main/urls.py
@@ -0,0 +1,15 @@
+#from django.conf.urls import url, include
+from django.urls import path, include
+from django.urls.resolvers import URLPattern
+from . import views
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+app_name = 'todo_main'
+
+urlpatterns = [
+    path(r'^$', views.Todo_main.as_view(), name='todo_main'),
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
